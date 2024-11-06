6a8784708e0bda2da9469f3645d07c3133e9476e
ChrysostomosC <153235540+ChrysostomosC@users.noreply.github.com>
2024-11-06 19:57:42 +0100

    Add files via upload
    Blogg med Vue JS
    Beskrivning
    Använd Vue JS för att bygga en personlig blogg.
    Använd eget innehåll eller Lorem Ipsum-text (https://www.lipsum.com/) för att bygga en startsida
    där alla blogginlägg listas, och separata undersidor för varje inlägg. På startsidan ska varje inlägg gå
    att klicka på för att visa hela inlägget på en egen sida.

---
+++ views/BloggsDetailsView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div v-if="blog">
+        <h1>{{ blog.title }}</h1>
+        <p>{{ blog.description }}</p>
+        <router-link to="/bloggs">Back to Blogs</router-link>
+    </div>
+    <div v-else>
+        <p>Blog not found. <router-link to="/bloggs">Back to Blogs</router-link></p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['title'],
+    data() {
+        return {
+            bloggs: [
+                {
+                    title: "One day at the park",
+                    description: "A nice sunny day at the park, having a picnic next to the duck lake and then playing games on the grass!",
+                },
+                {
+                    title: "My wife's misery",
+                    description: "My wife cannot handle the kids alone. I hope one day I can provide more help after I am done with my studies.",
+                },
+                {
+                    title: "Changes in life",
+                    description: "I am done with Zara, and now I am much calmer and happier than before.",
+                },
+            ],
+            blog: {},
+        };
+    },
+    created() {
+        this.blog = this.bloggs.find(b => b.title === this.title);
+    }
+};
+</script>
---
+++ views/BloggsView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>Welcome to my blogs</h1>
+        <p v-for="b in bloggs" :key="b.title">
+            <router-link :to="{ name: 'BloggsDetails', params: { title: b.title } }">
+                <b>{{ b.title }}</b>
+            </router-link>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            bloggs: [
+                {
+                    title: "One day at the park",
+                    description: "A nice sunny day at the park, having a picnic next to the duck lake and then playing games on the grass!",
+                },
+                {
+                    title: "My wife's misery",
+                    description: "My wife cannot handle the kids alone. I hope one day I can provide more help after I am done with my studies.",
+                },
+                {
+                    title: "Changes in life",
+                    description: "I am done with Zara, and now I am much calmer and happier than before.",
+                },
+            ]
+        };
+    }
+};
+</script>
